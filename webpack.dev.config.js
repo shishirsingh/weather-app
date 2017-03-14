7c4963974eae43f2bfa15a1366f8d90720f2da8f
Chauhan, Shishir (US - Hyderabad) <shichauhan@DELOITTE.com>
2017-03-14 17:36:26 +0530

    Server config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
 
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     filename: 'js/bundle.js',
     publicPath: '/dist/'
   },
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -28,6 +28,11 @@ module.exports = {
       }
     })
   ],
+    externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
 
   module: {
     loaders: [
